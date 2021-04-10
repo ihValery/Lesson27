073c938c3c2fe9e4c9a293c1b6939d2124c29acc
Valery <ihValery@gmail.com>
2021-04-10 22:28:33 +0300

    Стало учше

--- Lesson27/Controller/ViewController.swift
+++ Lesson27/Controller/ViewController.swift
@@ -8,15 +8,47 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet private var startPageLabel: UILabel! {
+        willSet {
+            newValue.text = "Стартовая страница"
+            newValue.font = .systemFont(ofSize: 30)
+            newValue.textColor = .gray
+        }
+    }
+    
+    @IBOutlet private var startPresentationLabel: UIButton! {
+        willSet {
+            newValue.setTitle("Старт презентации", for: .normal)
+            newValue.setTitleColor(.gray, for: .normal)
+            newValue.layer.cornerRadius = 13
+            newValue.titleLabel?.font = .systemFont(ofSize: 30)
+        }
+    }
+    
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         startPresentation()
     }
     
-    func startPresentation() {
-        if let pageVC = storyboard?.instantiateViewController(withIdentifier: "PageViewController") as? PageViewController {
-            present(pageVC, animated: true, completion: nil)
+    @IBAction private func startPresentationAction() {
+        
+        let userDefauls = UserDefaults.standard
+        userDefauls.set(false, forKey: "presentationWasViewed")
+        startPresentation()
+    }
+    
+    private func startPresentation() {
+        
+        let userDefaults = UserDefaults.standard
+        let presentationWasViewed = userDefaults.bool(forKey: "presentationWasViewed")
+        
+        if presentationWasViewed == false {
+            if let pageVC = storyboard?.instantiateViewController(withIdentifier: "PageViewController") as? PageViewController {
+                present(pageVC, animated: true, completion: nil)
+            }
         }
+        
+      
     }
 }
