d9002b653aaf2088c203658b2eceb517d032d273
Valery <ihValery@gmail.com>
2021-04-10 20:28:13 +0300

    @IBOutlet private var label: UILabel! { } Настройка оутлета через замыкание

--- Lesson27/Controller/PageViewController.swift
+++ Lesson27/Controller/PageViewController.swift
@@ -9,9 +9,33 @@ import UIKit
 
 class PageViewController: UIPageViewController {
 
+    let presentText = ["Хочешь покидать мячик? Но одному скучно?",
+                       "456",
+                       "789",
+                       "012"]
+    
+    let emojiCollection = ["🏀", "🥏", "🏈", "🏸"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        if let contentVC = showViewControllerAtIndex(0) {
+            setViewControllers([contentVC], direction: .forward, animated: true, completion: nil)
+        }
+    }
+    
+    func showViewControllerAtIndex(_ index: Int) -> ContentViewController? {
+        
+        guard index >= 0 else { return nil }
+        guard index < presentText.count else { return nil }
+        
+        guard let ContentVC = storyboard?.instantiateViewController(withIdentifier: "ContentViewController") as? ContentViewController else { return nil }
+        
+        ContentVC.presentText = presentText[index]
+        ContentVC.emoji = emojiCollection[index]
+        ContentVC.currenrPage = index
+        ContentVC.numberOfPage = presentText.count
+        
+        return ContentVC
     }
 }
