dd3d5dad1edb9cfbc5a0bb6ab95b77411b36aee3
Valery <ihValery@gmail.com>
2021-04-10 18:55:45 +0300

    Создали ContentViewController: UIViewController как шаблон PageViewController: UIPageViewController для управления самими страницами

---
+++ Lesson27/Controller/PageViewController.swift
@@ -0,0 +1,17 @@
+//
+//  PageViewController.swift
+//  Lesson27
+//
+//  Created by Валерий Игнатьев on 10.04.21.
+//
+
+import UIKit
+
+class PageViewController: UIPageViewController {
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
